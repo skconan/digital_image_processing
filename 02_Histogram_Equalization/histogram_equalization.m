d083bf0613c2ed1d01bac392e3b57545ec3518dc
skconan <supakit.kr@gmail.com>
2018-10-22 01:33:40 +0700

    refactor code

--- 02_Histogram_Equalization/histogram_equalization.m
+++ 02_Histogram_Equalization/histogram_equalization.m
@@ -1,23 +1,27 @@
 % Author : Supakit Kriangkhajorn
-% Follow me : skconan
+% Github : skconan
 
 function histogram_equalization()
-% Declare variable
+    % Declare variable
     path = strcat(pwd,'\images\');
     images_list = {'brightness.jpg' 'darkness.jpg' 'dark_bright.jpg'};
-    ct = 1;
+    
     for index = 1:3     
+        ct = 1;
         img_name = char(strcat(path,images_list(index)));
         i = imread(img_name);
         i = imresize(i,0.5);
         gray = rgb2gray(i);
         hist_equ = histeq(gray);
         
-        subplot(3,3,ct); subimage(gray(:,:));
+        figure;
+        subplot(2,2,ct); subimage(gray(:,:));
         ct = ct+1;
-        subplot(3,3,ct); subimage(hist_equ(:,:));
+        subplot(2,2,ct); subimage(gray(:,:));
         ct = ct+1;
-        subplot(3,3,ct); imhist(hist_equ(:,:));
+        subplot(2,2,ct); imhist(gray(:,:));
+        ct = ct+1;
+        subplot(2,2,ct); imhist(hist_equ(:,:));
         ct = ct+1;
     end
 end
