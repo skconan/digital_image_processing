34391b9182315146f5d320952c6e7d2d84b31e12
skconan <supakit.kr@gmail.com>
2018-10-22 01:14:38 +0700

    rewrite -> create func

--- 01_Intensity_Transformation/powerlaw_transformations.m
+++ 01_Intensity_Transformation/powerlaw_transformations.m
@@ -9,23 +9,22 @@ function powerlaw_transformations(image_name)
     gray = rgb2gray(img);
     gray = im2double(gray);
     mapping = zeros(1,256);
-    
+    y = 1;
     for i = 1:256
         mapping(1,i) = (i-1)/255;
     end
     
     
     sld = uicontrol('Style', 'slider',...
-        'Min',0.4,'Max',250,'Value',1,...
-        'Position', [850 500 500 30],...
+        'Min',0.4,'Max',250,'Value',10,...
+        'Position', [650 500 700 30],...
         'Callback', @trackbar_gamma); 
-    
-    function trackbar_gamma(source,event)
-        y = source.Value/10;
+    plot_result();
+    function plot_result()
         txt = uicontrol('Style','text',...
             'FontSize', 24,...
-            'Position',[900 550 120 40],...
-            'String',string(y));
+            'Position',[450 500 200 40],...
+            'String',strcat('Gamma: ',string(y)));
          
         output = gray.^y;     
         map_out = mapping.^y;
@@ -36,5 +35,9 @@ function powerlaw_transformations(image_name)
         subplot(2,2,2); subimage(output(:,:));
         subplot(2,2,3); imhist(output(:,:));
         subplot(2,2,4); plot(mapping,map_out);
+    end
+    function trackbar_gamma(source,event)
+        y = source.Value/10;
+        plot_result();
     end
 end
