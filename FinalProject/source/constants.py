cd509f5559fa374647a51d8b9a6999ac0889d1f7
skconan <supakit.kr@gmail.com>
2018-12-02 01:14:25 +0700

    init final project

---
+++ FinalProject/source/constants.py
@@ -0,0 +1,14 @@
+"""
+    File name: constants.py
+    Author: skconan
+    Date created: 2018/12/01
+    Python Version: 3.5
+"""
+
+import os
+
+path,_ = os.path.split(os.path.abspath(__file__))
+PATH_PJ,_ = os.path.split(path)
+PATH_VDO = PATH_PJ + "\videos"
+PATH_IMG = PATH_PJ + "\images"
+PATH_SRC = PATH_PJ + "\source"
